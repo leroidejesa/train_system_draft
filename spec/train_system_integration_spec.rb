020e1da299cd978b7b413b7d2f22e208b1defbf9
chase & leroi <student@epicodus.com>
2015-05-06 16:41:50 -0700

    add new train to train list, spec pass test.

--- spec/train_system_integration_spec.rb
+++ spec/train_system_integration_spec.rb
@@ -8,8 +8,10 @@ describe('path to add a new train', {:type => :feature}) do
   it('allows an operator to add a new train') do
     visit('/')
     click_link('Train Operators')
+    click_link('Add a train')
     fill_in('train_name', :with =>'MAX White')
     click_button('Add')
-    expect(page).to have_content('train master')
+    click_link('Home')
+    expect(page).to have_content('MAX White')
   end
 end
